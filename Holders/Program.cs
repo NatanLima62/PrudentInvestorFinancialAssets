fa1b5e04d643dbfb909a45d616dfe31f513d66d3
NatanLima <natanlimaoliveira62@gmail.com>
2024-03-07 19:22:03 -0300

    add sub

--- Holders/Program.cs
+++ Holders/Program.cs
@@ -6,59 +6,118 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        var url = "https://finance.yahoo.com/quote/AAPL/holders";
-        var httpClient = new HttpClient();
-        httpClient.DefaultRequestHeaders.Add("User-Agent", "teste");
-        var html = httpClient.GetStringAsync(url).Result;
-        var htmlDocument = new HtmlDocument();
-        htmlDocument.LoadHtml(html);
+        // var url = "https://finance.yahoo.com/quote/AAPL/holders";
+        // var httpClient = new HttpClient();
+        // httpClient.DefaultRequestHeaders.Add("User-Agent", "teste");
+        // var html = httpClient.GetStringAsync(url).Result;
+        // var htmlDocument = new HtmlDocument();
+        // htmlDocument.LoadHtml(html);
         
-        var majorHolders = htmlDocument.DocumentNode
-            .SelectNodes("//table[@class='W(100%) M(0) BdB Bdc($seperatorColor)']")
-            .Descendants("td")
-            .Where(node => 
-                node.GetAttributeValue("class", "").Equals("Py(10px) Va(m) Fw(600) W(15%)"))
-            .Select(node => node.InnerText)
-            .ToList();
-        
-        var tables = htmlDocument.DocumentNode
-            .SelectNodes("//table[@class='W(100%) BdB Bdc($seperatorColor)']")
-            .ToList();
+        // var majorHolders = htmlDocument.DocumentNode
+        //     .SelectNodes("//table[@class='W(100%) M(0) BdB Bdc($seperatorColor)']")
+        //     .Descendants("td")
+        //     .Where(node =>
+        //         node.GetAttributeValue("class", "").Equals("Py(10px) Va(m) Fw(600) W(15%)"))
+        //     .Select(node => node.InnerText)
+        //     .ToList();
+        //
+        // var tables = htmlDocument.DocumentNode
+        //     .SelectNodes("//table[@class='W(100%) BdB Bdc($seperatorColor)']")
+        //     .ToList();
+        //
+        // foreach (var table in tables)
+        // {
+        //     var tableHead = table.SelectSingleNode(".//thead");
+        //     var thValues = tableHead.Elements("tr")
+        //         .SelectMany(tr => tr.Elements("th")
+        //             .Where(th =>
+        //                 th.Attributes["class"]?.Value == "Ta(start) Fw(400) Py(6px)" ||
+        //                 th.Attributes["class"]?.Value == "Ta(end) Fw(400) Py(6px) Pstart(15px)"))
+        //         .Select(th => th.InnerText.Trim())
+        //         .ToList();
+        //
+        //     Console.WriteLine("Valores dos th:");
+        //     Console.WriteLine(string.Join(", ", thValues));
+        //
+        //
+        //     var tableBody = table.SelectSingleNode(".//tbody");
+        //
+        //     // Extrair os valores dos th no thead
+        //     // Extrair os valores dos td no tbody
+        //     var tdValues = tableBody.Elements("tr")
+        //         .Select(tr =>
+        //         {
+        //             var tdList = tr.Elements("td")
+        //                 .Where(td =>
+        //                     td.Attributes["class"]?.Value == "Ta(start) Pend(10px)" ||
+        //                     td.Attributes["class"]?.Value == "Ta(end) Pstart(10px)")
+        //                 .Select(td => td.InnerText.Trim())
+        //                 .ToList();
+        //
+        //             // Adicione o valor do primeiro td com a classe específica
+        //             tdList.Insert(0, tr.SelectSingleNode("td[@class='Py(10px) Ta(start)']")?.InnerText.Trim());
+        //
+        //             return tdList;
+        //         })
+        //         .ToList();
+        //
+        //     Console.WriteLine("Valores dos td:");
+        //     foreach (var tdList in tdValues)
+        //     {
+        //         Console.WriteLine(string.Join(", ", tdList));
+        //     }
+        // }
 
-        foreach (var table in tables)
+        ///////////
+        var url2 = "https://finance.yahoo.com/quote/AAPL/insider-transactions";
+        var httpClient2 = new HttpClient();
+        httpClient2.DefaultRequestHeaders.Add("User-Agent", "teste");
+        var html2 = httpClient2.GetStringAsync(url2).Result;
+        var htmlDocument2 = new HtmlDocument();
+        htmlDocument2.LoadHtml(html2);
+
+        // var insiderTransactions = htmlDocument2.DocumentNode
+        //     .SelectNodes("//table[@class='W(100%) M(0) BdB Bdc($seperatorColor)']")
+        //     .Descendants("td")
+        //     .Where(node =>
+        //         node.GetAttributeValue("class", "").Equals("Py(10px) Va(m) Fw(600) W(15%)"))
+        //     .Select(node => node.InnerText)
+        //     .ToList();
+
+        var tables2 = htmlDocument2.DocumentNode
+            .SelectNodes("//table[@class='W(100%) M(0)']")
+            .ToList();
+        
+        foreach (var table in tables2)
         {
             var tableHead = table.SelectSingleNode(".//thead");
             var thValues = tableHead.Elements("tr")
-                .SelectMany(tr => tr.Elements("th")
-                    .Where(th => 
-                        th.Attributes["class"]?.Value == "Ta(start) Fw(400) Py(6px)" ||
-                        th.Attributes["class"]?.Value == "Ta(end) Fw(400) Py(6px) Pstart(15px)"))
+                .SelectMany(tr => tr.Elements("th"))
                 .Select(th => th.InnerText.Trim())
                 .ToList();
-
+        
             Console.WriteLine("Valores dos th:");
             Console.WriteLine(string.Join(", ", thValues));
-            
-            
+        
+        
             var tableBody = table.SelectSingleNode(".//tbody");
-
+        
             // Extrair os valores dos th no thead
             // Extrair os valores dos td no tbody
             var tdValues = tableBody.Elements("tr")
                 .Select(tr =>
                 {
                     var tdList = tr.Elements("td")
-                        .Where(td => td.Attributes["class"]?.Value == "Ta(start) Pend(10px)" || td.Attributes["class"]?.Value == "Ta(end) Pstart(10px)")
                         .Select(td => td.InnerText.Trim())
                         .ToList();
-                    
+        
                     // Adicione o valor do primeiro td com a classe específica
-                    tdList.Insert(0, tr.SelectSingleNode("td[@class='Py(10px) Ta(start)']")?.InnerText.Trim());
-
+                    tdList.Insert(0, tr.SelectSingleNode("td[@class='Ta(end) BdB Bdc($seperatorColor)']")?.InnerText.Trim());
+        
                     return tdList;
                 })
                 .ToList();
-
+        
             Console.WriteLine("Valores dos td:");
             foreach (var tdList in tdValues)
             {
---
+++ Sustainability/Program.cs
@@ -0,0 +1,50 @@
+﻿using HtmlAgilityPack;
+
+namespace Sustainability;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var url = "https://finance.yahoo.com/quote/AAPL/sustainability";
+        var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Add("User-Agent", "teste");
+        var html = httpClient.GetStringAsync(url).Result;
+        var htmlDocument = new HtmlDocument();
+        htmlDocument.LoadHtml(html);
+        
+        var totalEsgRiskScore = htmlDocument.DocumentNode
+            .SelectNodes("//div[@class='Pos(r) H(55px) smartphone_Mb(15px)']")
+            .Descendants("div")
+            .Where(node =>
+                node.GetAttributeValue("class", "").Equals("C($tertiaryColor) Fz(s)") ||
+                node.GetAttributeValue("class", "").Equals("Fz(36px) Fw(600) D(ib) Mend(5px)") ||
+                node.GetAttributeValue("class", "").Equals("D(ib)") ||
+                node.GetAttributeValue("class", "").Equals("smartphone_D(n)"))
+            .Select(node => node.InnerText)
+            .ToList();
+        
+        var elements = htmlDocument.DocumentNode
+            .SelectNodes("//div[@class='Va(t) D(ib) W(22%) smartphone_W(33%) Wow(bw) Bxz(bb) Px(5px)']")
+            .Descendants("div")
+            .Where(node =>
+                node.GetAttributeValue("class", "").Equals("D(ib) Fz(23px) smartphone_Fz(22px) Fw(600)"))
+            .Select(node => node.InnerText)
+            .ToList();
+    }
+}
+
+public class EsgRiskRating
+{
+    public TotalEsgRiskScore TotalEsgRiskScore { get; set; } = null!;
+    public string EnvironmentRiskScore { get; set; } = null!;
+    public string SocialRiskScore { get; set; } = null!;
+    public string GovernanceRiskScore { get; set; } = null!;
+}
+
+public class TotalEsgRiskScore
+{
+    public string Value { get; set; } = null!;
+    public string Rate { get; set; } = null!;
+    public string RiskLevel { get; set; } = null!;
+}
